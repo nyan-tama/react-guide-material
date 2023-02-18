4636bc078bef7e3b6b698bfbd5c49c22929376fb
nyan-tama <shake.flake.love@gmail.com>
2023-02-19 00:09:27 +0900

    fi 04_020

--- 05_state_and_event/src/020_useState/start/Example.js
+++ 05_state_and_event/src/020_useState/start/Example.js
@@ -1,8 +1,16 @@
-const Example = () => {
+import { useState } from "react"
 
+const Example = () => {
+  let [val, setVal] = useState()
   return (
     <>
-      
+      <input type="text"
+        onChange={(e) => {
+          // const setFn = valArry[1]
+          setVal(e.target.value)
+        }}
+      /> ... {val}
+
     </>
   );
 };
