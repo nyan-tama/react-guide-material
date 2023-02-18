13bcbdc43af1ce43da29aa6e174c25c90dd8a97c
nyan-tama <shake.flake.love@gmail.com>
2023-02-18 18:11:46 +0900

    fi 04_110

--- 04_react_basic/src/110_props_children/start/Example.js
+++ 04_react_basic/src/110_props_children/start/Example.js
@@ -2,15 +2,26 @@ import Profile from "./components/Profile";
 import Container from "./components/Container";
 
 const profile = [
-  { name: "Takashi", age: 19, country: "Japan", color: "green" },
-  { name: "Jane", age: 28, country: "UK", color: "blue" },
+  { name: "Takashi", age: 19, country: "Japan", color: "" },
+  { name: "Jane", age: 28, country: "UK", color: "red" },
 ];
 
 const Example = () => {
   return (
     <div>
-      <Container title="Childrenとは？" />
-    </div>
+      <Container title="Childrenとは？" children={
+        [
+          <Profile key={profile[0].name} {...profile[0]} />,
+          <Profile key={profile[1].name} {...profile[1]} />
+        ]
+      }>
+      </Container>
+      <Container title="個別"
+        first={<Profile key={profile[1].name} {...profile[1]} />}
+        second={<Profile key={profile[0].name} {...profile[0]} />}
+      >
+      </Container>
+    </div >
   );
 };
 
