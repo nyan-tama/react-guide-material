281161f172e0e3f8819e0b1301cf5316e6a796d4
nyan-tama <shake.flake.love@gmail.com>
2023-02-21 23:38:30 +0900

    fi 11_050

---
+++ 11_hooks_p1/src/040_useContext_with_state/.start/components/OtherChild.js
@@ -0,0 +1,17 @@
+import { useContext } from "react";
+import { MyContext } from "./Example";
+const OtherChild = () => {
+  const [, setState] = useContext(MyContext);
+  const clickHandler = (e) => {
+    setState((prev) => prev + 1);
+  };
+
+  return (
+    <div>
+      <h3>他の子コンポーネント</h3>
+      <button onClick={clickHandler}>+</button>
+    </div>
+  );
+};
+
+export default OtherChild;
