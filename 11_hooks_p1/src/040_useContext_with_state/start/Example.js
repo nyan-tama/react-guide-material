0091189ab90498dc886d03224d05e08f67d85ae2
nyan-tama <shake.flake.love@gmail.com>
2023-02-21 21:21:21 +0900

    fi 11_040

--- 11_hooks_p1/src/040_useContext_with_state/start/Example.js
+++ 11_hooks_p1/src/040_useContext_with_state/start/Example.js
@@ -1,14 +1,17 @@
-import { createContext } from "react";
+import { createContext, useState } from "react";
 import Child from "./components/Child";
 import OtherChild from "./components/OtherChild";
-export const MyContext = createContext("hello");
+export const MyContext = createContext();
 
 const Example = () => {
+  const [state, setState] = useState(0);
+
   return (
-    <>
+    // Providerで囲むことで、子コンポーネントに値を渡すことができる
+    <MyContext.Provider value={[state, setState]}>
       <Child />
       <OtherChild />
-    </>
+    </MyContext.Provider>
   );
 };
 
