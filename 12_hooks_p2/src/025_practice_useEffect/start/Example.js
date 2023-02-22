def13a0176ead23a877348626ddbfbf55e2f7943
nyan-tama <shake.flake.love@gmail.com>
2023-02-22 16:43:20 +0900

    fi 12_025

--- 12_hooks_p2/src/025_practice_useEffect/start/Example.js
+++ 12_hooks_p2/src/025_practice_useEffect/start/Example.js
@@ -1,7 +1,11 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 const Example = () => {
   const [checked, setChecked] = useState(false);
+
+  useEffect(() => {
+    checked && alert('checked!');
+  }, [checked]);
 
   return (
     <>
