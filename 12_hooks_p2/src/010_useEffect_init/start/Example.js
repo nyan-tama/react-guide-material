139343260fd667d2ecd2ecaf29901830dfd52d0f
nyan-tama <shake.flake.love@gmail.com>
2023-02-22 16:34:11 +0900

    fi 12_010

--- 12_hooks_p2/src/010_useEffect_init/start/Example.js
+++ 12_hooks_p2/src/010_useEffect_init/start/Example.js
@@ -1,5 +1,23 @@
+import { useState, useEffect } from "react"
+
 const Example = () => {
-  return <></>;
+  const [time, setTime] = useState(0)
+
+  useEffect(() => {
+    console.log("useEffect one time");
+    window.setInterval(() => {
+      setTime(prev => prev + 1)
+    }, 1000)
+  }, [])
+
+  return (<>
+    <h3>
+      <time>
+        {time}
+      </time>
+      <span>秒経過</span>
+    </h3>
+  </>);
 };
 
 export default Example;
--- 12_hooks_p2/src/020_useEffect_update/start/Example.js
+++ 12_hooks_p2/src/020_useEffect_update/start/Example.js
@@ -9,13 +9,18 @@ const Example = () => {
       setTime(prev => prev + 1);
     }, 1000);
   }, [])
-  
+
+  useEffect(() => {
+    document.title = "counter:" + time
+    window.localStorage.setItem("time-key", time)
+  }, [time])
+
   return (
     <h3>
       <time>{time}</time>
       <span>秒経過</span>
     </h3>
-    );
+  );
 };
 
 export default Example;
